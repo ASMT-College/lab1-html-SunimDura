f2992b7df863bdef0e15b5272c92d7016524a412
SunimDura <sunimdura60@gmail.com>
2025-01-12 17:48:51 +0545

    assignment

---
+++ google/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google </title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="logo">
+        <img src="googlelogo.png" alt="Google Logo" width="272" height="92"> 
+    </div>
+    <form action="https://www.google.com/search" method="get">
+        <div class="search-container">
+            <input type="text" name="q" class="search-bar" placeholder="Search Google..." required>
+            <div class="buttons">
+                <button class="btn">Google Search</button>
+                <button class="btn">I'm Feeling Lucky</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
